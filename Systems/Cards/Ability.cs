14b3e3b96a7fb432d9b6d58a63851cc19f23d41e
Alex Wong <alexwong024@gmail.com>
2018-04-08 21:42:41 -0400

    base classes

---
+++ Systems/Cards/Ability.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//The basis for Card Actions and Item Actions
+public class Ability {
+    public enum Type {
+        Damage,
+        GrantBuff,
+        DrawCards,
+    }
+}
+
+//Passive Abilities on Allies, Equipment
+public class PassiveAbility {
+    
+}
+
+public class Buff {
+    
+}
--- Systems/Cards/Card.cs
+++ Systems/Cards/Card.cs
@@ -2,44 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-//Base class for any card
-
-public enum Attribute {
-    Strength,
-    Dexterity,
-    Intellect,
-    Charisma
-}
-
+//Skill Cards that can be drawn
 public class Card {
-    public enum Type {
-        Skill,
-        Ally,
-        Item
-    }
+    private string card_name;
+    private Attribute category_attribute;
 
-    string card_name;
-    string card_cost;
-    Attribute category_attribute;
-
-    //Require:
     //Card Art
-}
 
-public class SkillCard : Card {
-    //Require:
-    //Card Abilities
-}
-
-public class AllyCard : Card {
-    int health;
-    int attack;
-
-    //Require:
-    //Ally Abilities
-}
-
-public class ItemCard : Card {
-    //Require:
-    //Item Abilities
+    private EventType eventType;
 }
